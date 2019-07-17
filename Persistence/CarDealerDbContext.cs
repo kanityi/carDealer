949421adea9c325080ac85abba7b12b879dd3421
Sibu <sibu@Admins-MacBook-Pro.local>
2019-07-17 20:12:55 +0200

    Adding DbContext, also connection string

---
+++ Persistence/CarDealerDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace carDealer.Persistence
+{
+    public class CarDealerDbContext : DbContext
+    {
+        public CarDealerDbContext(DbContextOptions<CarDealerDbContext> options) : base (options)
+        {
+            
+        }
+    }
+}
